65dd929641a734eabbabd330a12e4d845a4b902b
banbutsunohoshi <165324678+banbutsunohoshi@users.noreply.github.com>
2025-12-21 18:03:15 +0300

    add menu display functionality

--- cli/menu.py
+++ cli/menu.py
@@ -1,1 +1,9 @@
+from cli.views import display_message
 
+
+def show_main_menu() -> str:
+    display_message("\nГлавное меню")
+    display_message("1 — Новая прогулка")
+    display_message("2 — История прогулок")
+    display_message("3 — Выход")
+    return input("Выберите пункт: ").strip()
