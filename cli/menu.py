255e1ac56130f94d0585a07408f1d7b70e26ba78
banbutsunohoshi <165324678+banbutsunohoshi@users.noreply.github.com>
2025-12-23 02:13:08 +0300

    Update menu.py

--- cli/menu.py
+++ cli/menu.py
@@ -1,9 +1,21 @@
+from dataclasses import dataclass
+from typing import Callable
+
 from cli.views import display_message
 
 
+@dataclass
+class MainMenu:
+    input_func: Callable[[str], str] = input
+    display_func: Callable[[str], None] = display_message
+
+    def show(self) -> str:
+        self.display_func("\nГлавное меню")
+        self.display_func("1 — Новая прогулка")
+        self.display_func("2 — История прогулок")
+        self.display_func("3 — Выход")
+        return self.input_func("Выберите пункт: ").strip()
+
+
 def show_main_menu() -> str:
-    display_message("\nГлавное меню")
-    display_message("1 — Новая прогулка")
-    display_message("2 — История прогулок")
-    display_message("3 — Выход")
-    return input("Выберите пункт: ").strip()
+    dreturn MainMenu().show()
